2c493f5f057ea2defa78255fb7e2b5d0c4cdbc34
KDash201 <KVonMadison@Yahoo.com>
2022-02-09 19:48:12 -0500

    Dashboard route reconfig

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,94 +1,75 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Movie, User } = require("../models");
+const withAuth = require("../utils/auth");
 
-router.get('/', withAuth, (req, res) => {
-  Post.findAll({
+router.get("/", withAuth, (req, res) => {
+  Movie.findAll({
     where: {
       // use the ID from the session
-      user_id: req.session.user_id
+      user_id: req.session.user_id,
     },
     attributes: [
-      'id',
-      'post_url',
-      'title',
-      'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      "id",
+      "title",
+      "post_url",
+      [sequelize.literal("(SELECT * FROM movie)")],
     ],
     include: [
       {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-        include: {
-          model: User,
-          attributes: ['username']
-        }
-      },
-      {
         model: User,
-        attributes: ['username']
-      }
-    ]
+        attributes: ["username"],
+      },
+    ],
   })
-    .then(dbPostData => {
+    .then((dbMovieData) => {
       // serialize data before passing to template
-      const posts = dbPostData.map(post => post.get({ plain: true }));
-      res.render('dashboard', { posts, loggedIn: true });
+      const movies = dbMovieData.map((movie) => movie.get({ plain: true }));
+      res.render("dashboard", { movies, loggedIn: true });
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
 });
 
-router.get('/edit/:id', withAuth, (req, res) => {
-  Post.findOne({
+router.get("/:id", withAuth, (req, res) => {
+  Movie.findOne({
     where: {
-      id: req.params.id
+      id: req.params.id,
     },
     attributes: [
-      'id',
-      'post_url',
-      'title',
-      'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      "id",
+      "title",
+      "created_at",
+      [sequelize.literal("(SELECT COUNT (*) FROM movie WHERE movie.id)")],
     ],
     include: [
       {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-        include: {
-          model: User,
-          attributes: ['username']
-        }
-      },
-      {
         model: User,
-        attributes: ['username']
-      }
-    ]
+        attributes: ["username"],
+      },
+    ],
   })
-    .then(dbPostData => {
-      if (!dbPostData) {
-        res.status(404).json({ message: 'No post found with this id' });
+    .then((dbMovieData) => {
+      if (!dbMovieData) {
+        res.status(404).json({ message: "No movie found with this id" });
         return;
       }
 
       // serialize the data
-      const post = dbPostData.get({ plain: true });
+      const movie = dbMovieData.get({ plain: true });
 
       // pass data to template
-      res.render('edit-post', {
-        post,
-        loggedIn: true
-       });
+      res.render("dashboard", {
+        movie,
+        loggedIn: true,
+      });
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
-
 });
 
 module.exports = router;
