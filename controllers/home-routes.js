7f0d3bb3fc2da9c760f319742eb16b102bc87db6
KDash201 <KVonMadison@Yahoo.com>
2022-02-14 01:12:45 -0500

    updated home route and css

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -37,39 +37,7 @@ const { Movie, User } = require("../models");
 //     });
 // });
 
-router.get("/movies/:id", (req, res) => {
-  Movie.findOne({
-    where: {
-      id: req.params.id,
-    },
-    attributes: ["id", "title", "release", "created_at"],
-  })
-    .then((dbMovieData) => {
-      if (!dbMovieData) {
-        res.status(404).json({ message: "No movie found with this id" });
-        return;
-      }
-
-      // serialize the data
-      const movie = dbMovieData.get({ plain: true });
-
-      // pass data to template
-      res.render("single-post", {
-        movie,
-        loggedIn: true,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
-router.get("/login", (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect("/");
-    return;
-  }
+router.get("/", (req, res) => {
   res.render("login");
 });
 
